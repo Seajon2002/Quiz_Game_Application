06c91f70c5fc5e2b4f96087555c62bea31427db3
Ektearul Haque <151764873+Seajon2002@users.noreply.github.com>
2024-05-21 12:34:33 +0600

    Add files via upload

---
+++ Score.java
@@ -0,0 +1,50 @@
+package quiz_application;
+
+import java.awt.*;
+import javax.swing.*;
+import java.awt.event.*;
+
+public class Score extends JFrame implements ActionListener {
+
+    Score(String name, int score) {
+        setBounds(400, 150, 750, 550);
+        getContentPane().setBackground(Color.WHITE);
+        setLayout(null);
+        
+        ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/score.png"));
+        Image i2 = i1.getImage().getScaledInstance(200, 150, Image.SCALE_DEFAULT);
+        ImageIcon i3 = new ImageIcon(i2);
+        JLabel image = new JLabel(i3);
+        image.setBounds(200, 25, 300, 250);
+        add(image);
+        
+        JLabel heading = new JLabel("Thank you " + name + "! for playing Quiz Craze");
+        heading.setBounds(125, 25, 700, 30);
+        heading.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        add(heading);
+        
+        JLabel lblscore = new JLabel("Your score is " + score);
+        lblscore.setBounds(275, 250, 300, 30);
+        lblscore.setFont(new Font("Tahoma", Font.PLAIN, 26));
+        add(lblscore);
+        
+        JButton submit = new JButton("Play Again!");
+        submit.setBounds(300, 320, 120, 30);
+        submit.setBackground(new Color(30, 144, 255));
+        submit.setForeground(Color.WHITE);
+        submit.addActionListener(this);
+        add(submit);
+        
+        setVisible(true);
+    }
+    
+    @Override 
+    public void actionPerformed(ActionEvent ae) {
+        setVisible(false);
+        new Login();
+    }
+
+    public static void main(String[] args) {
+        new Score("User", 0);
+    }
+}
